0b1adc456ce4a85cb131ffef77a678ce33416dd7
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-03-22 17:44:01 -0500

    Fix linter errors

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 let books = [];
 
-const deleteBook = (bookTitle) => {
-  const result = books.filter((book) => book.title !== bookTitle);
-  books = result;
-  localStorage.setItem('books', JSON.stringify(books));
-  renderBooks(books);
-};
-
-const addBook = (title, author) => {
-  const bookList = document.getElementById('book-list');
-  const book = document.createElement('div');
-  const bookTitle = document.createElement('p');
-  bookTitle.textContent = `Title: ${title}`;
-  const bookAuthor = document.createElement('p');
-  bookAuthor.textContent = `Author: ${author}`;
-  const removeBtn = document.createElement('button');
-  removeBtn.textContent = 'Remove';
-
-  removeBtn.addEventListener('click', () => {
-    deleteBook(title);
-  });
-
-  const bookHr = document.createElement('hr');
-
-  book.append(bookTitle, bookAuthor, removeBtn, bookHr);
-  bookList.append(book);
-};
-
 const renderBooks = (books) => {
+  const addBook = (title, author) => {
+    const bookList = document.getElementById('book-list');
+    const book = document.createElement('div');
+    const bookTitle = document.createElement('p');
+    bookTitle.textContent = `Title: ${title}`;
+    const bookAuthor = document.createElement('p');
+    bookAuthor.textContent = `Author: ${author}`;
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+
+    const deleteBook = (bookTitle) => {
+      const result = books.filter((book) => book.title !== bookTitle);
+      books = result;
+      localStorage.setItem('books', JSON.stringify(books));
+      renderBooks(books);
+    };
+
+    removeBtn.addEventListener('click', () => {
+      deleteBook(title);
+    });
+
+    const bookHr = document.createElement('hr');
+
+    book.append(bookTitle, bookAuthor, removeBtn, bookHr);
+    bookList.append(book);
+  };
+
   const bookList = document.getElementById('book-list');
   bookList.innerHTML = '';
   books.forEach((book) => {
