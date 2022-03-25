204efded14fe9e51143729ddee902f0243f393e7
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-03-25 11:45:50 -0500

    Create class method to add books

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-class BookObject {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-
-  add() {
-    books.push(this);
-  }
-
-  remove(bookTitle) {
-    books.filter((b) => {
-      b.title !== bookTitle;
-    });
-  }
-}
-
 const bookList = document.getElementById('book-list');
 const addBtn = document.getElementById('add-button');
 const bookTitleInput = document.getElementById('title');
@@ -34,57 +17,74 @@ function removeBook() {
 }
 
 
-function addBook() {
-  // To check if a book is repeated
-  function checkTitleBook(book) {
-    return book.title === bookTitleInput.value;
-  }
-  
+function createAndAppend(title, author) {
+  // Create
   const book = document.createElement('div');
   const bookTitle = document.createElement('p');
   const bookAuthor = document.createElement('p');
   const removeBtn = document.createElement('button');
   const bookHr = document.createElement('hr');
 
+  // Classes
   book.classList.add('book');
   bookTitle.classList.add('book-title');
-  
-    
+
+  // Add Content
+  i += 1;
+  bookTitle.textContent = `Title: ${title}`;
+  bookAuthor.textContent = `Author: ${author}`;
+  removeBtn.textContent = `Remove ${i}`;
+
+  // Append
+  book.append(bookTitle, bookAuthor, removeBtn, bookHr);
+  bookList.append(book);
+
+  removeBtn.addEventListener('click', removeBook);
+}
+
+// To check if a book is repeated
+function checkTitleBook(book) {
+  return book.title === bookTitleInput.value;
+}
+
+
+class BookObject {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  add() {
+    books.push(this);
+    localStorage.setItem('books', JSON.stringify(books));
+    createAndAppend(this.title, this.author);
+  }
+}
+
+
+
+function addBook() {
   if(
     bookTitleInput.value !== ''
     && bookAuthorInput.value !== ''
     ) {
-      
+
       if(books.some(checkTitleBook)) {
         alertMessage.innerHTML = 'This book already exists';
         alertMessage.style.display = 'block';
-      } else {
-        // Push Object to Array
-        const newBook = new BookObject(bookTitleInput.value, bookAuthorInput.value);
-        books.push(newBook);
 
-        // localStorage
-        localStorage.setItem('books', JSON.stringify(books));
-        
-        i += 1;
-        bookTitle.textContent = `Title: ${newBook.title}`;
-        bookAuthor.textContent = `Author: ${newBook.author}`;
-        removeBtn.textContent = `Remove ${i}`;
-        
-        book.append(bookTitle, bookAuthor, removeBtn, bookHr);
-        bookList.append(book);
-        
-        
-        removeBtn.addEventListener('click', removeBook);
-        
+      } else {
+        const newBook = new BookObject(bookTitleInput.value, bookAuthorInput.value);
+        newBook.add();
+
         bookTitleInput.value = bookTitleInput.defaultValue;
         bookAuthorInput.value = bookAuthorInput.defaultValue;
-        
+
         alertMessage.style.display = 'none';
       }
-      
+
     } else {
-      alertMessage.innerHTML = 'Fill out both fields';
+      alertMessage.innerHTML = 'Add text to both fields';
       alertMessage.style.display = 'block';
   }
 };
