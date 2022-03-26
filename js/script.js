be76c16317fe4feb6b0d79ba6921b97b557ad20e
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-03-26 00:45:52 -0500

    Fix linter errors

--- js/script.js
+++ js/script.js
@@ -6,19 +6,16 @@ const alertMessage = document.getElementById('alert-message');
 
 let books = [];
 
-
 function bookBackground(book, element) {
-  if(element % 2 === 0) {
+  if (element % 2 === 0) {
     book.style.backgroundColor = 'rgb(214, 214, 214)';
-    
   } else {
     book.style.backgroundColor = 'azure';
   }
 }
 
-
 function removeBook() {
-  // console.log(books.findIndex((book) => 
+  // console.log(books.findIndex((book) =>
   // book.title === this.parentElement.children[1].innerHTML.split(' ')[1]));
 
   books = books.filter((book) => `Title: ${book.title}` !== this.parentNode.childNodes[0].innerHTML);
@@ -30,8 +27,6 @@ function removeBook() {
   const booksArr = document.querySelectorAll('.book');
   booksArr.forEach((book, i) => bookBackground(book, i % 2 !== 0));
 }
-
-
 
 function createAndAppend(title, author) {
   // Create
@@ -58,10 +53,9 @@ function createAndAppend(title, author) {
   // Append
   book.append(bookTitle, bookAuthor, removeBtn);
   bookList.append(book);
-  
+
   removeBtn.addEventListener('click', removeBook);
 }
-
 
 function loadContent() {
   books = JSON.parse(localStorage.getItem('books'));
@@ -70,13 +64,10 @@ function loadContent() {
   });
 }
 
-
 // To check if a book is repeated
 function checkTitleBook(book) {
   return book.title === bookTitleInput.value;
 }
-
-
 
 class BookObject {
   constructor(title, author) {
@@ -91,41 +82,30 @@ class BookObject {
   }
 }
 
-
-
-function f() {
-  console.log(books);
-}
-window.addEventListener('click', f);
-
-
-
 function addBook() {
-  if(
+  if (
     bookTitleInput.value !== ''
     && bookAuthorInput.value !== ''
-    ) {
-      if(books.some(checkTitleBook)) {
-        alertMessage.innerHTML = 'This book already exists';
-        alertMessage.style.display = 'block';
-        alertMessage.style.backgroundColor = 'rgb(165, 192, 200)';
-      } else {
-        const newBook = new BookObject(bookTitleInput.value, bookAuthorInput.value);
-        newBook.add();
-
-        
-        bookTitleInput.value = bookTitleInput.defaultValue;
-        bookAuthorInput.value = bookAuthorInput.defaultValue;
-
-        alertMessage.style.display = 'none';
-      }
-
-    } else {
-      alertMessage.innerHTML = 'Add text to both fields';
+  ) {
+    if (books.some(checkTitleBook)) {
+      alertMessage.innerHTML = 'This book already exists';
       alertMessage.style.display = 'block';
-      alertMessage.style.backgroundColor = 'rgb(185, 140, 228)';
+      alertMessage.style.backgroundColor = 'rgb(165, 192, 200)';
+    } else {
+      const newBook = new BookObject(bookTitleInput.value, bookAuthorInput.value);
+      newBook.add();
+
+      bookTitleInput.value = bookTitleInput.defaultValue;
+      bookAuthorInput.value = bookAuthorInput.defaultValue;
+
+      alertMessage.style.display = 'none';
+    }
+  } else {
+    alertMessage.innerHTML = 'Add text to both fields';
+    alertMessage.style.display = 'block';
+    alertMessage.style.backgroundColor = 'rgb(185, 140, 228)';
   }
-};
+}
 
 function highlightMessage() {
   alertMessage.style.transform = 'scale(1.05)';
@@ -141,4 +121,3 @@ addBtn.addEventListener('click', addBook);
 addBtn.addEventListener('mousedown', highlightMessage);
 addBtn.addEventListener('mouseup', noHighlightMessage);
 loadContent();
-
